192b80c6fb4e3b07fd8a00c8fc4b21e35633f3cd
Jairatnopparat@gmail.com <nzbz21ghj>
2016-12-30 13:37:39 +0700

    add project to repository

---
+++ Filter_LINQtoSQL/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvProduct = New System.Windows.Forms.DataGridView()
+        Me.btnFilter = New System.Windows.Forms.Button()
+        Me.btnRestore = New System.Windows.Forms.Button()
+        Me.txtFilter = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.dgvProduct, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvProduct
+        '
+        Me.dgvProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvProduct.Location = New System.Drawing.Point(12, 12)
+        Me.dgvProduct.Name = "dgvProduct"
+        Me.dgvProduct.Size = New System.Drawing.Size(499, 296)
+        Me.dgvProduct.TabIndex = 0
+        '
+        'btnFilter
+        '
+        Me.btnFilter.Location = New System.Drawing.Point(316, 330)
+        Me.btnFilter.Name = "btnFilter"
+        Me.btnFilter.Size = New System.Drawing.Size(75, 23)
+        Me.btnFilter.TabIndex = 1
+        Me.btnFilter.Text = "Fine"
+        Me.btnFilter.UseVisualStyleBackColor = True
+        '
+        'btnRestore
+        '
+        Me.btnRestore.Location = New System.Drawing.Point(416, 330)
+        Me.btnRestore.Name = "btnRestore"
+        Me.btnRestore.Size = New System.Drawing.Size(75, 23)
+        Me.btnRestore.TabIndex = 2
+        Me.btnRestore.Text = "Restore"
+        Me.btnRestore.UseVisualStyleBackColor = True
+        '
+        'txtFilter
+        '
+        Me.txtFilter.Location = New System.Drawing.Point(120, 333)
+        Me.txtFilter.Name = "txtFilter"
+        Me.txtFilter.Size = New System.Drawing.Size(180, 20)
+        Me.txtFilter.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(72, 336)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Filter"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(523, 387)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFilter)
+        Me.Controls.Add(Me.btnRestore)
+        Me.Controls.Add(Me.btnFilter)
+        Me.Controls.Add(Me.dgvProduct)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvProduct, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgvProduct As DataGridView
+    Friend WithEvents btnFilter As Button
+    Friend WithEvents btnRestore As Button
+    Friend WithEvents txtFilter As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Filter_LINQtoSQL/Form1.vb
@@ -0,0 +1,29 @@
+﻿Option Explicit On
+Option Strict On
+Public Class Form1
+
+    Dim db As New dbNorthwindDataContext()
+    Dim IProduct As IQueryable(Of Product)
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        IProduct = From p In db.Products
+        dgvProduct.DataSource = IProduct.ToList()
+    End Sub
+
+    Private Sub btnFilter_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFilter.Click
+        Dim ps = From p In IProduct
+                 Where (p.ProductID.ToString().Contains(txtFilter.Text.Trim()) OrElse p.ProductName.Contains(txtFilter.Text.Trim()))
+        If ps.Count() > 0 Then
+            dgvProduct.DataSource = ps.ToList()
+        Else
+            dgvProduct.DataSource = Nothing
+            MessageBox.Show("เงื่อนไขที่คุณระบุไม่มี !!", "ผลการกรองข้อมูล", MessageBoxButtons.OK)
+            txtFilter.Text = ""
+            txtFilter.Focus()
+        End If
+    End Sub
+
+    Private Sub btnRestore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRestore.Click
+        dgvProduct.DataSource = IProduct.ToList()
+    End Sub
+End Class
